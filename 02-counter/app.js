5b5bd8f8e5c8b14f49066bcf30951f005ffa5967
Javier Aguila <sjaca10@gmail.com>
2024-11-11 21:12:41 -0600

    Counter project

---
+++ 02-counter/app.js
@@ -0,0 +1,28 @@
+// Set initial count
+let count = 0
+
+const value = document.querySelector('#value')
+const buttons = document.querySelectorAll('.btn')
+
+buttons.forEach(function(button) {
+  button.addEventListener('click', function(event) {
+    const styles = event.currentTarget.classList
+    if (styles.contains('decrease')) {
+      count--
+    } else if (styles.contains('increase')) {
+      count++
+    } else {
+      count = 0
+    }
+
+    if (count > 0) {
+      value.style.color = 'green'
+    } else if (count < 0) {
+      value.style.color = 'red'
+    } else {
+      value.style.color = 'black'
+    }
+
+    value.textContent = count
+  })
+})
